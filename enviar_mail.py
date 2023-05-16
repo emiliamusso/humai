b23243ac11a74426af5baccadb245b73829fbe61
Emilia Musso <100376004+emiliamusso@users.noreply.github.com>
2023-05-16 19:20:10 -0300

    Update enviar_mail.py

--- enviar_mail.py
+++ enviar_mail.py
@@ -3,8 +3,8 @@ import smtplib
 from email.message import EmailMessage
 
 # Usuario y contraseña
-user_gmail = "emmimusso@gmail.com"
-password_gmail = "hdhvdlyjgspkcemp"
+user_gmail = os.env('USER_GMAIL')
+password_gmail = os.env('PASSWORD_GMAIL')
 
 def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
   msg = EmailMessage()
@@ -27,3 +27,6 @@ def send_notifying_mail(mail_user: str = "", mail_password: str = "") -> None:
 
 
 send_notifying_mail(user_gmail, password_gmail)
+
+  if __name__ == "__main__":
+      enviar_mail(user_gmail, password_gmail) # modificar segun se llame tu funcion y parametros
